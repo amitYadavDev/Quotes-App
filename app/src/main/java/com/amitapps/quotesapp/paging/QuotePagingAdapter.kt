02f7463bd95bf07ab8fd7ef5b8884c24195614f8
amitYadavDev <amit8955461017@gmail.com>
2024-03-26 14:45:37 +0530

    paging adapter impl

---
+++ app/src/main/java/com/amitapps/quotesapp/paging/QuotePagingAdapter.kt
@@ -0,0 +1,46 @@
+package com.amitapps.quotesapp.paging
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.amitapps.quotesapp.R
+import com.amitapps.quotesapp.data.model.Result
+
+class QuotePagingAdapter : PagingDataAdapter<Result, QuotePagingAdapter.QuoteViewHolder>(COMPARATOR) {
+
+    class QuoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val quote = itemView.findViewById<TextView>(R.id.quote)
+    }
+
+    override fun onBindViewHolder(holder: QuotePagingAdapter.QuoteViewHolder, position: Int) {
+        val item = getItem(position)
+        item?.let {
+            holder.quote.text = it.content
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): QuotePagingAdapter.QuoteViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.quote_item, parent, false)
+        return QuoteViewHolder(view)
+    }
+
+    companion object {
+        private val COMPARATOR = object : DiffUtil.ItemCallback<Result>() {
+            override fun areItemsTheSame(oldItem: Result, newItem: Result): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Result, newItem: Result): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+}
